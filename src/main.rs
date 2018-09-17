60efa341d3971f4bc3395b85f7c5d3eac3698525
Sergey Grankin <sgrankin@gmail.com>
2018-09-17 00:21:17 -0400

    chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 extern crate cgmath;
+extern crate collision;
 extern crate image;
 
+use cgmath::*;
+use collision::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<Error>> {
     let width = 200;
     let height = 100;
 
+    let lower_left_corner = Vector3::new(-2.0, -1.0, -1.0);
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, 2.0, 0.0);
+    let horizontal = Vector3::new(4.0, 0.0, 0.0);
+
     let mut imgbuf = image::RgbImage::new(width, height);
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let col = [x as f32 / width as f32, (height - y) as f32 / height as f32, 0.2];
+        let u = (x as f32) / (width as f32);
+        let v = ((height - y) as f32) / (height as f32);
+        let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+        let col = color(&r);
         *pixel =
             image::Rgb([(col[0] * 255.99) as u8, (col[1] * 255.99) as u8, (col[2] * 255.99) as u8]);
     }
 
     image::ImageRgb8(imgbuf).save("out.png")?;
     Ok(())
+}
+
+fn color(r: &Ray3<f32>) -> Vector3<f32> {
+    let t = (r.direction.y + 1.0) * 0.5;
+    (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
 }
