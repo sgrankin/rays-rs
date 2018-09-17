5e8dbc4760721fdc747288e614542357893ecba0
Sergey Grankin <sgrankin@gmail.com>
2018-09-16 23:40:38 -0400

    chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+extern crate cgmath;
 extern crate image;
 
 use std::error::Error;
-use std::fs;
 
 fn main() -> Result<(), Box<Error>> {
     let width = 200;
@@ -9,10 +9,9 @@ fn main() -> Result<(), Box<Error>> {
 
     let mut imgbuf = image::RgbImage::new(width, height);
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = x as f32 / width as f32;
-        let g = (height - y) as f32 / height as f32;
-        let b = 0.2;
-        *pixel = image::Rgb([(r * 255.99) as u8, (g * 255.99) as u8, (b * 255.99) as u8]);
+        let col = [x as f32 / width as f32, (height - y) as f32 / height as f32, 0.2];
+        *pixel =
+            image::Rgb([(col[0] * 255.99) as u8, (col[1] * 255.99) as u8, (col[2] * 255.99) as u8]);
     }
 
     image::ImageRgb8(imgbuf).save("out.png")?;
