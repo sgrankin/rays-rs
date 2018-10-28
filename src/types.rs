8e6e24de3c7ab8aeaf1e45137efcb057e43a7729
Sergey Grankin <sgrankin@gmail.com>
2018-10-27 20:58:00 -0400

    rename cgmath types so that they don't sneak in again

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,11 @@
 pub use cgmath::{Array, ElementWise, EuclideanSpace, InnerSpace, MetricSpace, VectorSpace, Zero};
-pub use cgmath::{Point3, Vector2, Vector3};
+pub use cgmath::{Point3 as _Point3, Vector2 as _Vector2, Vector3 as _Vector3};
 
 pub use std::f64::consts::PI;
 pub use std::f64::MAX as FLOAT_MAX;
 pub use std::f64::MIN as FLOAT_MIN;
 
 pub type Float = f64;
-pub type Vector2f = Vector2<Float>;
-pub type Vector3f = Vector3<Float>;
-pub type Point3f = Point3<Float>;
+pub type Vector2f = _Vector2<Float>;
+pub type Vector3f = _Vector3<Float>;
+pub type Point3f = _Point3<Float>;
