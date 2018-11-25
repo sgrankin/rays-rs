14644ab0094371ef135467b3d9c787467ceb98ef
Sergey Grankin <sgrankin@gmail.com>
2018-11-24 23:07:57 -0500

    actually prepare for filters

--- src/framebuf.rs
+++ src/framebuf.rs
@@ -1,53 +1,42 @@
 use crate::types::*;
-
-#[derive(Copy, Clone)]
-pub struct Pixel {
-    rgb: Vector3f,
-    weights: Float,
-}
-
-impl Pixel {
-    fn new() -> Pixel {
-        Pixel { rgb: Vector3f::zero(), weights: 0.0 }
-    }
-    pub fn add_sample(&mut self, rgb: Vector3f, weight: Float) {
-        self.rgb += rgb;
-        self.weights += weight;
-    }
-
-    pub fn to_rgb(&self) -> [u8; 3] {
-        let mut rgb = self.rgb;
-        if self.weights > 0.0 {
-            rgb /= self.weights
-        }
-        let v = rgb.map(|v| (v * 255.99) as u8);
-        [v.x, v.y, v.z]
-    }
-}
+use std::collections::BTreeMap;
 
 pub struct FrameBuf {
     pub width: usize,
     pub height: usize,
-    pixels: Vec<Pixel>,
+    pixels: BTreeMap<(usize, usize), Vec<(Point2f, Vector3f)>>,
 }
 
 impl FrameBuf {
     pub fn new(width: usize, height: usize) -> FrameBuf {
-        FrameBuf { width, height, pixels: vec![Pixel::new(); height * width] }
+        FrameBuf { width, height, pixels: BTreeMap::new() }
     }
 
     pub fn mk_image(&self) -> image::RgbImage {
         let mut buf = image::RgbImage::new(self.width as u32, self.height as u32);
         buf.enumerate_pixels_mut().for_each(|(x, y, p)| {
-            *p = image::Rgb(
-                self.pixels[(x as usize + self.width * (self.height - 1 - y as usize))].to_rgb(),
-            );
+            let empty = vec![];
+            // Box filter: ignore subpixel locations; add all with weight=1.0
+            let pixels =
+                self.pixels.get(&(x as usize, self.height - 1 - y as usize)).unwrap_or(&empty);
+            let pixel = pixels.iter().fold(Vector3f::zero(), |sum, (_, p)| sum + p)
+                / (pixels.len() as Float);
+            *p = image::Rgb(FrameBuf::to_rgb(pixel));
         });
         buf
     }
+
+    fn to_rgb(rgb: Vector3f) -> [u8; 3] {
+        let v = rgb.map(|v| (v * 255.99) as u8);
+        [v.x, v.y, v.z]
+    }
+
     pub fn add_sample(&mut self, pixel: Point2u, subpixel: Point2f, rgb: Vector3f) {
-        // box filter: ignore subpixel; weight is 1
-        self.pixels[pixel.x + self.width * pixel.y].add_sample(rgb, 1.0)
+        let k = (pixel.x, pixel.y);
+        if !self.pixels.contains_key(&k) {
+            self.pixels.insert(k, vec![]);
+        }
+        self.pixels.get_mut(&k).unwrap().push((subpixel, rgb));
     }
 
     pub fn enum_pixels(&self) -> Vec<Point2u> {
